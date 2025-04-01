eb9f4bfa57661c18eb9444aade6f9b3e11eefbf6
bigelle <ruslan.ok04@gmail.com>
2025-04-01 22:51:28 +0500

    auth kinda works

--- auth-service/cmd/main.go
+++ auth-service/cmd/main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	"github.com/bigelle/student-portal/proto/auth"
 	"google.golang.org/grpc"
 )
 
-type authService struct{}
+type authService struct {
+	auth.UnimplementedAuthServer
+}
+
+func (s authService) Login(context.Context, *auth.LoginRequest) (*auth.LoginResponse, error) {
+	return &auth.LoginResponse{
+		Name:    "it works",
+		UserId:  42,
+		Session: "session",
+		Csrf:    "csrf",
+	}, nil
+}
 
 func main() {
 	lis, err := net.Listen("tcp", ":8081")
@@ -16,4 +29,10 @@ func main() {
 	}
 
 	server := grpc.NewServer()
+	service := &authService{}
+
+	auth.RegisterAuthServer(server, service)
+	if err := server.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 }
--- auth-service/go.mod
+++ auth-service/go.mod
@@ -5,9 +5,10 @@ go 1.24.1
 require google.golang.org/grpc v1.71.1
 
 require (
-	golang.org/x/net v0.34.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
-	google.golang.org/protobuf v1.36.4 // indirect
+	github.com/bigelle/student-portal/proto v0.0.0-20250401173943-992e8e6060c4
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250324211829-b45e905df463 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
